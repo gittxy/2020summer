67188b86fe46037427cb2022ef9ca94b21fd0c2e
txy <1324269532@qq.com>
2020-07-01 12:12:33 +0800

    初始化本地仓库

---
+++ src/test/java/com/example/bx_web/BxWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bx_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BxWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
