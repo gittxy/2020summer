563705a5de2cd99b5296e4a1dc42fb9e6ec30d56
chuyaflouds <wf04261999>
2020-07-01 16:36:20 +0800

    初始化本地仓库

---
+++ src/main/java/com/example/bx_web/BxWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.bx_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BxWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BxWebApplication.class, args);
+    }
+
+}
